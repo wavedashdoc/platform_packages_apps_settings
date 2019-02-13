277b71feccc39d3ef18415f13f287c2a59d2a58d
camcory <camcory1@gmail.com>
2019-02-13 07:51:45 -0500

    Revert "SimSettings: Handle NoClassDefFoundError on devices without telephony-ext"
    This reverts commit 1ca65c549baa4266d0f8ffbe91ab39369a79b1d1.

--- src/com/android/settings/sim/SimSettings.java
+++ src/com/android/settings/sim/SimSettings.java
@@ -68,7 +68,6 @@ import com.android.settings.search.Indexable;
 
 import org.codeaurora.internal.IExtTelephony;
 
-import java.lang.NoClassDefFoundError;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -135,11 +134,7 @@ public class SimSettings extends RestrictedSettingsFragment implements Indexable
         mSubscriptionManager = SubscriptionManager.from(getActivity());
         final TelephonyManager tm =
                 (TelephonyManager) getActivity().getSystemService(Context.TELEPHONY_SERVICE);
-        try {
-            mExtTelephony = IExtTelephony.Stub.asInterface(ServiceManager.getService("extphone"));
-        } catch (NoClassDefFoundError ex) {
-            // ignore, device does not compile telephony-ext.
-        }
+        mExtTelephony = IExtTelephony.Stub.asInterface(ServiceManager.getService("extphone"));
 
         addPreferencesFromResource(R.xml.sim_settings);
 
