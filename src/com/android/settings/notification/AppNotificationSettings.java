a2f59a2f8e53600bf859576fa8e697cdab7510c0
Raff Tsai <rafftsai@google.com>
2019-03-08 16:19:30 +0800

    Do not allow draw on top for App notification settings
    Fixes: 119115683
    Test: manual
    Change-Id: Ib7b878a23b4a99171c58b5de992fb87feca8a28a
    Merged-In: Ib7b878a23b4a99171c58b5de992fb87feca8a28a
    (cherry picked from commit fe86a2a51f0b8e01e5f595bd60edc594eef58aba)
    (cherry picked from commit e4800786cff0df47392d0954d700743a1f6cfc8f)

--- src/com/android/settings/notification/AppNotificationSettings.java
+++ src/com/android/settings/notification/AppNotificationSettings.java
@@ -17,6 +17,8 @@
 package com.android.settings.notification;
 
 import android.app.Activity;
+import static android.view.WindowManager.LayoutParams.PRIVATE_FLAG_HIDE_NON_SYSTEM_OVERLAY_WINDOWS;
+
 import android.app.NotificationChannel;
 import android.app.NotificationChannelGroup;
 import android.app.NotificationManager;
@@ -34,6 +36,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Switch;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.aosip.owlsnest.preference.CustomSeekBarPreference;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
@@ -95,6 +99,9 @@ public class AppNotificationSettings extends NotificationSettingsBase {
     @Override
     public void onResume() {
         super.onResume();
+
+        getActivity().getWindow().addPrivateFlags(PRIVATE_FLAG_HIDE_NON_SYSTEM_OVERLAY_WINDOWS);
+        android.util.EventLog.writeEvent(0x534e4554, "119115683", -1, "");
 
         if (mUid < 0 || TextUtils.isEmpty(mPkg) || mPkgInfo == null) {
             Log.w(TAG, "Missing package or uid or packageinfo");
@@ -244,6 +251,14 @@ public class AppNotificationSettings extends NotificationSettingsBase {
                 return true;
             }
         });
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        final Window window = getActivity().getWindow();
+        final WindowManager.LayoutParams attrs = window.getAttributes();
+        attrs.privateFlags &= ~PRIVATE_FLAG_HIDE_NON_SYSTEM_OVERLAY_WINDOWS;
+        window.setAttributes(attrs);
     }
 
     private void addHeaderPref() {
